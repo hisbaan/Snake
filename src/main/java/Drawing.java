daf5565e19150a11112c674fad6e62369bb0a44c
Hisbaan <34548959+hisbaan@users.noreply.github.com>
2019-08-06 08:51:25 -0400

    Added movement. Does not work yet.

--- src/main/java/Drawing.java
+++ src/main/java/Drawing.java
@@ -4,6 +4,7 @@
 //Name: Hisbaan Noorani
 //
 //This program 
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -16,5 +17,17 @@ public class Drawing extends JPanel {
         Graphics2D g = (Graphics2D) g1;
 
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g.setColor(Color.black);
+
+        g.fillRect(0, 0, 800, 800);
+
+        g.setColor(Color.white);
+
+        for (int y = 0; y < 20; y++) {
+            for (int x = 0;x < 20; x++) {
+                if(Snake.snake[x][y][0] == 1) g.fillRect((800 / 20) * x, (800 / 2) * y, (800 / 20), (800 / 20));
+            }
+        }
     }
 }
--- src/main/java/Snake.java
+++ src/main/java/Snake.java
@@ -9,16 +9,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Snake implements ActionListener {
+public class Snake implements ActionListener, KeyListener, WindowListener {
     JFrame mainMenuFrame = new JFrame("Main Menu");
-    JPanel bottomPannel = new JPanel();
+    JPanel mainMenuBottomPannel = new JPanel();
     JButton startGameButton = new JButton("Star Game");
+
+    JFrame gameFrame = new JFrame("Snake");
+    JPanel gameBottomPannel = new JPanel();
+    JButton gameBackButton = new JButton("Back");
+    Drawing board = new Drawing();
+
+    char direction = ' ';
+    static int[][][] snake = new int[20][20][2];
+
+    Timer movement;
 
     public static void main(String[] args) {
         new Snake();
     }
 
     Snake() {
+        movement = new Timer(100, e -> {
+            move();
+            board.validate();
+            board.repaint();
+        });
         //setup timer for movement
         mainMenu();
     }
@@ -28,25 +43,153 @@ public class Snake implements ActionListener {
         mainMenuFrame.setLayout(new BorderLayout());
         mainMenuFrame.setResizable(false);
 
-        mainMenuFrame.add(bottomPannel, BorderLayout.SOUTH);
-        bottomPannel.setLayout(new GridLayout(1, 3));
-        bottomPannel.add(startGameButton);
+        mainMenuFrame.add(mainMenuBottomPannel, BorderLayout.SOUTH);
+        mainMenuBottomPannel.setLayout(new GridLayout(1, 3));
+        mainMenuBottomPannel.add(startGameButton);
 
+        if (startGameButton.getActionListeners().length < 1) startGameButton.addActionListener(this);
+
+        mainMenuFrame.setVisible(true);
+        gameFrame.setVisible(false);
     }
 
     public void startGame() {
+        gameFrame.setSize(800, 800);
+        gameFrame.setLayout(new BorderLayout());
+        gameFrame.setResizable(false);
+
+        gameFrame.add(gameBottomPannel, BorderLayout.SOUTH);
+
+        gameBottomPannel.setLayout(new GridLayout(1, 3));
+        gameBottomPannel.add(gameBackButton);
+        if (gameBackButton.getActionListeners().length < 1) gameBackButton.addActionListener(this);
+
+        gameFrame.add(board, BorderLayout.CENTER);
         /*
-        * array that tracks the snake's movement
-        * find a way to add the the end of the array
-        * maybe notation of how many turns it has been and set a disappear timer
-        * 3d array (x, y and time existed)
-        * if you pick up an apple, increase the time required before disappearing.  */
+         * array that tracks the snake's movement
+         * find a way to add the the end of the array
+         * maybe notation of how many turns it has been and set a disappear timer
+         * 3d array (x, y and time existed)
+         * if you pick up an apple, increase the time required before disappearing.  */
+
+        snake[10][10][0] = 1;
+
+        movement.start();
+
+        mainMenuFrame.setVisible(false);
+        gameFrame.setVisible(true);
+    }
+
+    public void move() {
+        switch (direction) {
+            case 'n':
+                north();
+                break;
+            case 's':
+                south();
+                break;
+            case 'e':
+                east();
+                break;
+            case 'w':
+                west();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void north() {
+        for (int y = 19; y > -1; y--) {
+            for (int x = 0; x < 20; x++) {
+                if (snake[x][y][0] == 1) {
+                    snake[x][y][0] = 0;
+                    snake[x][y - 1][0] = 1;
+                }
+            }
+        }
+    }
+
+    public void south() {
+
+    }
+
+    public void east() {
+
+    }
+
+    public void west() {
+
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == startGameButton) {
+        if (e.getSource() == startGameButton) {
             startGame();
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getSource() == gameFrame) {
+            if (e.getKeyCode() == KeyEvent.VK_UP) {
+                direction = 'n';
+            }
+            if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                direction = 's';
+            }
+            if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                direction = 'e';
+            }
+            if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                direction = 'w';
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
     }
 }
